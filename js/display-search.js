b5b67ca48de1f1182a14687f7b3876f174a9df35
Caitlin Swickard <caitlin6029@gmail.com>
2022-03-03 15:47:19 -0700

    completed fetch api for search

--- js/display-search.js
+++ js/display-search.js
@@ -1,8 +1,29 @@
+// grabbing form input from html 
+const searchFormEl = document.querySelector('#search-form');
 // grabbing html elements for search results
 const resultTextEl = document.querySelector("#result-text");
 const resultContentEl = document.querySelector("#result-content");
-// grabbing form input from html 
-const searchFormEl = document.querySelector('#search-form');
+
+
+
+// function to capture info from the search form
+function handleSearchFormSubmit(event) {
+  event.preventDefault();
+
+  // grabbing input values from form
+  let searchInputVal = document.querySelector('#search-input').value;
+  let formatInputVal = document.querySelector('#format-input').value;
+
+  if (!searchInputVal) {
+    alert('You need a search input value!');
+    return;
+  }
+  // calling the API and giving it the search values from the form
+  searchApi(searchInputVal, formatInputVal)
+}
+
+searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
 
 
 // api GET request with params for query/search and format/search
@@ -28,26 +49,24 @@ function searchApi(query, format) {
       // write query to page so user knows what they are viewing
       resultTextEl.textContent = res.search.query;
       console.log(res);
+
+      // if no results found on search change html to the h3
+      if (!res.results.length) {
+        console.log('No results found');
+        resultContentEl.innerHTML = '<h3>No results found, search again!</h3>'
+      } else {
+        resultContentEl.textContent = '';
+        for (let i = 0; i < res.results.length; i++) {
+          // return (res.results[i])
+        }
+      }
     })
+
+    .catch(function (err) {
+      console.log(err);
+    });
 }
 
 
 
 
-// function to capture info from the search form
-function handleSearchFormSubmit(event) {
-  event.preventDefault();
-
-  // grabbing input values from form
-  let searchInputVal = document.querySelector('#search-input').value;
-  let formatInputVal = document.querySelector('#format-input').value;
-
-  if (!searchInputVal) {
-    alert('You need a search input value!');
-    return;
-  }
-  // calling the API and giving it the search values from the form
-  searchApi(searchInputVal, formatInputVal)
-}
-
-searchFormEl.addEventListener('submit', handleSearchFormSubmit);
